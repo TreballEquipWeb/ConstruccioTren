fb073c87cb030ffcdd6798c388793ef7c18cecd8
EricRubioSanchez <u6103664@campus.udg.edu>
2026-05-03 20:36:13 +0200

    #14 Add calcularEstrelles method to SistemaEstrelles
    Introduce calcularEstrelles to compute 0–3 stars for a level. It accepts {victoria, accionsUsades = 0, accionsIdeals = this.criteriBase, recursosRestants = 0}, returns 0 when not victorious, 3 when actions <= ideal and recursosRestants > 0, 2 when actions <= ideal + 2, otherwise 1. The method also updates this.estrellesObtingudes with the computed value.

--- src/classes/SistemaEstrelles.js
+++ src/classes/SistemaEstrelles.js
@@ -10,4 +10,32 @@ export class SistemaEstrelles {
     this.estrellesObtingudes = 0
   }
 
+  /**
+   * Calcula el nombre d'estrelles obtingudes.
+   * @param {Object} params
+   * @param {boolean} params.victoria
+   * @param {number} [params.accionsUsades=0]
+   * @param {number} [params.accionsIdeals=this.criteriBase]
+   * @param {number} [params.recursosRestants=0]
+   * @returns {number} 0..3 estrelles
+   */
+  calcularEstrelles({ victoria, accionsUsades = 0, accionsIdeals = this.criteriBase, recursosRestants = 0 }) {
+    if (!victoria) {
+      this.estrellesObtingudes = 0
+      return 0
+    }
+
+    if (accionsUsades <= accionsIdeals && recursosRestants > 0) {
+      this.estrellesObtingudes = 3
+      return 3
+    }
+
+    if (accionsUsades <= accionsIdeals + 2) {
+      this.estrellesObtingudes = 2
+      return 2
+    }
+
+    this.estrellesObtingudes = 1
+    return 1
+  }
 }
