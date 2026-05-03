c81c916cc407ec5d487feb01d618c1c261b207e8
EricRubioSanchez <u6103664@campus.udg.edu>
2026-05-03 20:39:35 +0200

    Initialize game and sample actions in PlayScene
    Import Joc, Nivell and TIPOS_CASILLA and update PlayScene.create to instantiate a sample level (mapaInicial) and start a game. The code creates a Nivell with initial rails and action limits, calls iniciarJoc on a new Joc, performs example player actions (talarArbre, destruirObstacle, colocarRail) on specific tiles, finalizes the game (finalitzarPartida) and logs the game and final state. This provides an initial runtime scenario for testing gameplay logic and map interactions.

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { Joc, Nivell, TIPOS_CASILLA } from '../classes/index.js';
 export class PlayScene extends Phaser.Scene {
   constructor() {
     super({ key: 'PlayScene' });
@@ -9,7 +10,39 @@ export class PlayScene extends Phaser.Scene {
   }
 
   create() {
-    // Create the game objects for this scene
+    const mapaInicial = [
+      [TIPOS_CASILLA.INICI, TIPOS_CASILLA.PLA, TIPOS_CASILLA.BOSC, TIPOS_CASILLA.PLA, TIPOS_CASILLA.META],
+      [TIPOS_CASILLA.PLA, TIPOS_CASILLA.OBSTACLE, TIPOS_CASILLA.PLA, TIPOS_CASILLA.BOSC, TIPOS_CASILLA.PLA],
+      [TIPOS_CASILLA.PLA, TIPOS_CASILLA.PLA, TIPOS_CASILLA.PLA, TIPOS_CASILLA.PLA, TIPOS_CASILLA.PLA]
+    ]
+
+    this.nivell = new Nivell({
+      nom: 'Nivell de prova',
+      mapaInicial,
+      railsInicials: 2,
+      limitsAccions: {
+        tales: 1,
+        destruccions: 1,
+        rails: 2
+      }
+    })
+
+    this.joc = new Joc().iniciarJoc(this.nivell)
+    console.log('Joc iniciat:', this.joc)
+    const casellaBosc = this.joc.mapa.obtenirCasella(0, 2)
+    const casellaObstacle = this.joc.mapa.obtenirCasella(1, 1)
+    const casellaPla = this.joc.mapa.obtenirCasella(0, 1)
+    const casellaPla1 = this.joc.mapa.obtenirCasella(0, 2)
+    const casellaPla2 = this.joc.mapa.obtenirCasella(0, 3)
+
+    this.joc.jugador.talarArbre(casellaBosc)
+    this.joc.jugador.destruirObstacle(casellaObstacle)
+    this.joc.jugador.colocarRail(casellaPla)
+    this.joc.jugador.colocarRail(casellaPla1)
+    this.joc.jugador.colocarRail(casellaPla2)
+
+    this.estadoPartida = this.joc.finalitzarPartida(3)
+    console.log('Estado de partida:', this.estadoPartida)
   }
 
   update() {
